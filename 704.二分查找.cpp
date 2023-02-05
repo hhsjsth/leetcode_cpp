f6b133db89f350cc6348fde21f31881e3ddd2447
hhsjsth <is.gutaorui@outlook.com>
2023-02-06 00:32:00 +0800

    二分查找红蓝区间

--- 704.二分查找.cpp
+++ 704.二分查找.cpp
@@ -2,39 +2,31 @@
  * @lc app=leetcode.cn id=704 lang=cpp
  *
  * [704] 二分查找
- * 方法二: 左闭右开写法二
+ * 方法三: 左开右开写法
  */
 
 // @lc code=start
 class Solution {
   public:
     int search(vector<int> &nums, int target) {
-        // 左闭右开, 说明左边选得到, 右边选不到
-        int l = 0;
+        // 左开右开, 参考红蓝区间 https://www.bilibili.com/video/av841423368/
+        int l = -1;
         int h = nums.size();
-        // 左闭右开和左开右闭不一样了, 所以可以使用移位, 关于为什么左开右闭不能移位, 可以查看文件历史
-        int m = l + h >> 1;
+        // 左开右开为红蓝区间, 极限情况为红蓝区间碰撞后查找失败跳出循环, 不可能出现 l + h 为负数的情况, 因此移位不会出现负数, 不会导致数组越界
+        // 下面这种写法可以防止越界
+        int m = l + (h - l >> 1);
 
-        // 左闭右开, 说明只要 l == h 立刻退出循环
-        while (l < h) {
-            // 如果中间元素小于 target, 说明 target 在中间元素的右边
-            // 此时 m 下标的元素可以忽略了, 但是 m + 1 下标的元素不能忽略
-            // 由于是左闭右开, 因此 l 更新为 m + 1, 这样区间 [l, h) 就能将 m + 1 下标的元素包含进去
+        while (l < h - 1) {
             if (nums[m] < target) {
-                l = m + 1;
-            }
-            // 如果中间元素大于 target, 说明 target 在中间元素的左边
-            // 此时 m 下标的元素可以忽略了, 但是 m - 1 下标的元素不能忽略
-            // 由于是左开右闭, 因此 h 更新为 m, 这样区间 [l, h) 才能将 m - 1 下标的元素包含进去
-            else if (nums[m] > target) {
+                l = m;
+            } else if (nums[m] > target) {
                 h = m;
             } else if (nums[m] == target) {
                 return m;
             }
-            m = l + h >> 1;
+            m = l + (h - l >> 1);
         }
 
-        // 夹逼到最后只剩下左闭右开的区间已经没有元素
         return -1;
     }
 };

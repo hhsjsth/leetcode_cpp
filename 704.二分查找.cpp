4b6fd8c55374e2b731af8ea8354d51f674d8f773
hhsjsth <is.gutaorui@outlook.com>
2023-02-06 00:37:40 +0800

    左闭右闭

--- 704.二分查找.cpp
+++ 704.二分查找.cpp
@@ -2,25 +2,24 @@
  * @lc app=leetcode.cn id=704 lang=cpp
  *
  * [704] 二分查找
- * 方法三: 左开右开写法
+ * 方法四: 左闭右闭写法
  */
 
 // @lc code=start
 class Solution {
   public:
     int search(vector<int> &nums, int target) {
-        // 左开右开, 参考红蓝区间 https://www.bilibili.com/video/av841423368/
-        int l = -1;
-        int h = nums.size();
+        int l = 0;
+        int h = nums.size() - 1;
         // 左开右开为红蓝区间, 极限情况为红蓝区间碰撞后查找失败跳出循环, 不可能出现 l + h 为负数的情况, 因此移位不会出现负数, 不会导致数组越界
-        // 下面这种写法可以防止越界
+        // 下面这种写法可以防止整数越界
         int m = l + (h - l >> 1);
 
-        while (l < h - 1) {
+        while (l <= h) {
             if (nums[m] < target) {
-                l = m;
+                l = m + 1;
             } else if (nums[m] > target) {
-                h = m;
+                h = m - 1;
             } else if (nums[m] == target) {
                 return m;
             }

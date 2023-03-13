f24d68a85bd7ec25041a009bd7f143fd665624c6
hhsjsth <is.gutaorui@outlook.com>
2023-03-13 21:22:38 +0800

    移除元素方法二错误

--- 27.移除元素.cpp
+++ 27.移除元素.cpp
@@ -1,23 +1,31 @@
 /*
  * @lc app=leetcode.cn id=27 lang=cpp
  *
- * [27] 移除元素
+ * [27] 移除元素 方法二 优化的向中靠拢的双指针
  */
 
 // @lc code=start
 class Solution {
   public:
     int removeElement(vector<int> &nums, int val) {
-        int n = nums.size();
         int left = 0;
-        // 该方法需要遍历数组最多两遍, 一遍是 right 遍历完数组, 最多的情况是 left 也要遍历完数组.
-        // 但是会遇到需要赋值的次数较多, 因为只要遇到一个值为 val, 则需要将右边全部的值全部向左移动.
-        for (int right = 0; right < n; right++) {
-            if (nums[right] != val) {
-                nums[left++] = nums[right];
+        int right = nums.size() - 1;
+        /* 以下这种方式, 遇到一个 val 值, 只需赋值一次, 比上个方法更优, 而不是遇到一个 val 就得将
+         * 右边的全部值都向左移动赋值
+         */
+        /* 但是遇到 [val, val, val] 这种情况反而比方法一更耗时间, 需要赋值很多次 */
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right--];
+            } else {
+                left++;
             }
         }
-        return left;
+        return left + 1;
+        /* 然而以上这种方法是错误的, 因为如果 nums 的长度为 0, 则应该返回 -1
+         * 但是此时 left + 1 为 1, 已经超过数组长度, 因此错误
+         * 错误解决需要使用左开右闭的思想和方法, 见下一个提交
+         */
     }
 };
 // @lc code=end

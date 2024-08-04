455eae52cc7c33310d71584cf6648ff2fb92c764
hhsjsth <is.gutaorui@outlook.com>
2024-08-04 11:53:13 +0800

    合并区间两种做法

---
+++ 3.无重复字符的最长子串.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string s) {
+        // 哈希集合，记录每个字符是否出现过
+        unordered_set<char> occ;
+        int n = s.size();
+        // 右指针，初始值为 0，左闭右开，还没有开始移动
+        int rk = 0, ans = 0;
+        // 枚举左指针的位置
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.erase(s[i - 1]);
+            }
+            while (rk < n && !occ.count(s[rk])) {
+                // 不断地移动右指针
+                occ.insert(s[rk]);
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i);
+        }
+        return ans;
+    }
+};
+// @lc code=end
---
+++ 56.合并区间-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals) {
+        // 双指针
+        int point1 = 0, point2 = 0;
+        // 存储结果
+        vector<vector<int>> res;
+        // 先按照每个区间的首个元素排序
+        sort(intervals.begin(), intervals.end(),
+             [](vector<int> &a, vector<int> &b) { return a[0] < b[0]; });
+
+        // 维护两个指针, 收集合并的区间
+        point1 = intervals[0][0];
+        point2 = intervals[0][1];
+
+        // 这是我写的第一份。感觉循环写的不够优雅。
+        for (int i = 1; i < intervals.size(); i++) {
+            // 如果下一个区间的首个元素小于第二个指针, 则合并区间
+            if (intervals[i][0] <= point2) {
+                point2 = max(point2, intervals[i][1]);
+            } else {
+                // 否则收集当前两个指针所代表的区间。然后更新两个指针到下一个区间。
+                res.push_back({point1, point2});
+                point1 = intervals[i][0];
+                point2 = intervals[i][1];
+            }
+        };
+        res.push_back({point1, point2});
+        return res;
+    }
+}; // @lc code=end
